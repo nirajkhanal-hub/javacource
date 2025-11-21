2ed89a1103ecfe32239b9fb68b96e02438727260
nirajkhanal-hub <niraj.242019@ncit.edu.np>
2025-11-21 15:25:14 +0545

    add files via uploas

---
+++ datatype.java
@@ -0,0 +1,21 @@
+public class datatype {
+    public static void main (String[] args) {
+        int num1=1; //integer data type
+        short s=99; //short data type
+        byte by=8; //byte data type
+         long l=9999l; //long data type
+        float f=88.9f; //float data type
+        double d=009.443;   //double data type
+        char c='A';         //character data type    
+        boolean b=true;         //boolean data type
+          
+        System.out.println("Integer value: " + num1);               
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Long value: " + l);
+        System.out.println("Character value: " + c);
+        System.out.println("Boolean value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Byte value: " + by);
+    }
+}
---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable{
+     public static void main(String[] args)
+     {
+        int a=10; 
+        double b=99.9; 
+   
+        System.out.println(a+b);
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+   
+         
+     }
+}
