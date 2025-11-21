cb0b3a7dccae21b2dbc07646588e4374ad401382
nirajkhanal-hub <niraj.242019@ncit.edu.np>
2025-11-21 21:16:46 +0545

    RELATIONAL OPREATOR

---
+++ relationalopreator.java
@@ -0,0 +1,18 @@
+public class relationalopreator {
+public static void main(String[] args){
+    int x=10;
+    int y=20;
+     boolean result=x<y;
+    System.out.println(x==y); // false
+    System.out.println(x!=y); // true
+    System.out.println(x>=y); // false
+    System.out.println(x<=y); // true
+    System.out.println(x>y); // false
+    System.out.println(x<y); // true
+    System.out.println(result); // true
+    
+ 
+}
+    
+    
+}
